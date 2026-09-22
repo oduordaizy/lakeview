9b6e0bbecfb0b525c8f07a89e89c81b3764ad1fd
daisyoduor <oduordaizy@outlook.com>
2026-09-22 10:38:06 +0300

    changed navabar py to 0

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -33,7 +33,7 @@ export function SiteHeader() {
   return (
     <>
       <header
-        className="sticky top-0 z-50 w-full bg-white border-b border-slate-100 py-2"
+        className="sticky top-0 z-50 w-full bg-white border-b border-slate-100 py-0"
       >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between">
